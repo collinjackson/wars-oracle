04f4aa96c8e009fdefde7232d4e5fe8f5bc8d0a4
Collin Jackson <collin@nexus.xyz>
2025-12-06 23:39:39 -0800

    Fix React hook error by adding 'use client' directive

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,7 @@ import json
 import os
 from game_logic import get_reachable_cells, calculate_damage
 
-VERSION = "1.0.0"
+VERSION = "0.0.1"
 
 class GameAnalyzer:
     def __init__(self, map_file, units_file, rules_file, metadata_file=None):
